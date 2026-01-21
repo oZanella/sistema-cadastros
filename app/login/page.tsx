2050c44bd85ad158c32cea12bc55845be7509a89
oZanella <henriquezanella19@gmail.com>
2026-01-21 10:20:37 -0300

    WIP login - redirect in login local

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from "@/components/ui/login-form";
+import { redirect } from "next/navigation";
 
 export default function Login() {
+  return redirect("auth/login");
   return (
     <div>
       <div>
